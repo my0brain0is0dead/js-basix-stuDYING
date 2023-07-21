5538d529566fa1cf2c25b36e6c1e142cde507b36
my0brain0is0dead <118047676+my0brain0is0dead@users.noreply.github.com>
2023-07-21 10:57:00 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,18 @@
+//чтобы не повторять куски кода можно использовать функции
+//именованая функция
+function summation(a, b) {
+    const c = a + b
+    return c
+}
+
+
+let someA = 13
+let someB = 5
+let sum1 = summation(someA, someB)
+console.log(sum1)
+
+someA = 20
+someB = 543
+let sum2 = summation(someA, someB)
+console.log(sum2)
+
