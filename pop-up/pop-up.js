01e75d9721fc2c66926c01fd426b926f75b364fb
my0brain0is0dead <118047676+my0brain0is0dead@users.noreply.github.com>
2023-07-22 20:04:55 +0300

    Add files via upload

---
+++ pop-up/pop-up.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+  
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
