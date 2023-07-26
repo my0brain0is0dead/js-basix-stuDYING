335564ecb249db64df4d5b3f4e2a1cdc5d26518d
my0brain0is0dead <118047676+my0brain0is0dead@users.noreply.github.com>
2023-07-27 02:03:10 +0300

    Add files via upload

---
+++ switch case.js
@@ -0,0 +1,10 @@
+switch (5) {
+    case 5: 
+        console.log('5')
+        break
+    case 6:
+        console.log('6')
+        break
+    default:
+        console.log('not 5 and not 6')
+}
---
+++ ternary operator.js
@@ -0,0 +1,7 @@
+a = false
+
+a === true 
+? console.log('ne obmanyl') 
+: console.log('dymal bydet bas? naebal)0')
+
+// операндом может быть только выражение, никаких инструкций
